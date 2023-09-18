731924c55d105607804d1476e64d4eb7e8eec978
Praful Vats <prafulvats01@gmail.com>
2023-09-18 14:38:06 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,187 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
 
 function App() {
+  const [galleryStyle, setGalleryStyle] = useState({
+    transform: "translate(-4%, -20%)",
+    opacity: 0,
+  });
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [isTextVisible, setIsTextVisible] = useState(false);
+  const [showTiles, setShowTiles] = useState(false); 
+
+  useEffect(() => {
+    const gallery = document.getElementById("gallery");
+
+    const handleMouseMove = (e) => {
+      const mouseX = e.clientX;
+      const mouseY = e.clientY;
+
+      const xDecimal = mouseX / window.innerWidth;
+      const yDecimal = mouseY / window.innerHeight;
+
+      const maxX = gallery.offsetWidth - window.innerWidth;
+      const maxY = gallery.offsetHeight - window.innerHeight;
+
+      const panX = maxX * xDecimal * -1;
+      const panY = maxY * yDecimal * -1;
+
+      setGalleryStyle({
+        transform: `translate(${panX}px, ${panY}px)`,
+        opacity: 1,
+      });
+    };
+
+    setTimeout(() => {
+      setIsLoaded(true);
+    }, 2000);
+
+    setTimeout(() => {
+      setIsTextVisible(true);
+    }, 200);
+
+    // Add a timeout to make the tiles visible after a 1-second delay
+    setTimeout(() => {
+      setShowTiles(true);
+    }, 1000);
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={`fixed-text ${isTextVisible ? "visible" : ""}`}>palette</div>
+      <div
+        id="gallery"
+        className={`gallery ${showTiles ? "visible" : ""}`} // Conditionally apply visibility
+        style={galleryStyle}
+      >
+        <div className="tile">
+          <img
+            src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fHJhbmRvbSUyMG9iamVjdHN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=70"
+            alt="Tile 1"
+          />
+        </div>
+        <div className="tile">
+          <img
+            src="https://images.unsplash.com/photo-1515266591878-f93e32bc5937?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsdWV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=70"
+            alt="Tile 2"
+          />
+        </div>
+        <div className="tile">
+          <img
+            src="https://images.unsplash.com/photo-1587590227264-0ac64ce63ce8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tJTIwb2JqZWN0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=70"
+            alt="Tile 3"
+          />
+        </div>
+        <div className="tile">
+          <img
+            src="https://images.unsplash.com/photo-1520121401995-928cd50d4e27?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z3JlZW58ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=70"
+            alt="Tile 4"
+          />
+        </div>
+        <div className="tile">
+          <img
+            src="https://images.unsplash.com/photo-1511447333015-45b65e60f6d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHVycGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=70"
+            alt="Tile 5"
+          />
+        </div>
+        <div className="tile">
+          <img
+            src="https://images.unsplash.com/photo-1557800636-894a64c1696f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8b3JhbmdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=70"
+            alt="Tile 6"
+          />
+        </div>
+        <div className="tile">
+          <img
+            src="https://images.unsplash.com/photo-1520338258525-606b90f95b04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGRhcmslMjBibHVlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=70"
+            alt="Tile 7"
+          />
+        </div>
+        <div className="tile">
+          <img
+            src="https://images.unsplash.com/photo-1521127474489-d524412fd439?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTh8fHJhbmRvbSUyMG9iamVjdHN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=70"
+            alt="Tile 8"
+          />
+        </div>
+        <div className="tile">
+          <img
+            src="https://images.unsplash.com/photo-1598300042247-d088f8ab3a91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cmFuZG9tJTIwb2JqZWN0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=70"
+            alt="Tile 9"
+          />
+        </div>
+        <div className="tile">
+          <img
+            src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fHJhbmRvbSUyMG9iamVjdHN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=70"
+            alt="Tile 10"
+          />
+        </div>
+        <div className="tile">
+          <img
+            src="https://images.unsplash.com/photo-1515266591878-f93e32bc5937?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsdWV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=70"
+            alt="Tile 11"
+          />
+        </div>
+        <div className="tile">
+          <img
+            src="https://images.unsplash.com/photo-1587590227264-0ac64ce63ce8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tJTIwb2JqZWN0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=70"
+            alt="Tile 12"
+          />
+        </div>
+        <div className="tile">
+          <img
+            src="https://images.unsplash.com/photo-1520121401995-928cd50d4e27?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z3JlZW58ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=70"
+            alt="Tile 13"
+          />
+        </div>
+        <div className="tile">
+          <img
+            src="https://images.unsplash.com/photo-1511447333015-45b65e60f6d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHVycGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=70"
+            alt="Tile 14"
+          />
+        </div>
+        <div className="tile">
+          <img
+            src="https://images.unsplash.com/photo-1557800636-894a64c1696f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8b3JhbmdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=70"
+            alt="Tile 15"
+          />
+        </div>
+        <div className="tile">
+          <img
+            src="https://images.unsplash.com/photo-1520338258525-606b90f95b04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGRhcmslMjBibHVlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=70"
+            alt="Tile 16"
+          />
+        </div>
+        <div className="tile">
+          <img
+            src="https://images.unsplash.com/photo-1521127474489-d524412fd439?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTh8fHJhbmRvbSUyMG9iamVjdHN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=70"
+            alt="Tile 17"
+          />
+        </div>
+        <div className="tile">
+          <img
+            src="https://images.unsplash.com/photo-1598300042247-d088f8ab3a91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cmFuZG9tJTIwb2JqZWN0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=70"
+            alt="Tile 18"
+          />
+        </div>
+        <div className="tile">
+          <img
+            src="https://images.unsplash.com/photo-1521127474489-d524412fd439?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTh8fHJhbmRvbSUyMG9iamVjdHN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=70"
+            alt="Tile 19"
+          />
+        </div>
+        <div className="tile">
+          <img
+            src="https://images.unsplash.com/photo-1598300042247-d088f8ab3a91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cmFuZG9tJTIwb2JqZWN0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=70"
+            alt="Tile 20"
+          />
+        </div>
+      </div>
     </div>
   );
 }
